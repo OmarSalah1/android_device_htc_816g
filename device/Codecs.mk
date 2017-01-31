7d8f33bf1ed2919a30f19687c2406a776c08c0a1
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 04:01:04 +0530

    Device : Add Codecs.mk Configs

---
+++ device/Codecs.mk
@@ -0,0 +1,5 @@
+# Codecs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/codecs/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/codecs/platform.xml:system/etc/permissions/platform.xml \
+	$(LOCAL_PATH)/configs/codecs/media_profiles.xml:system/etc/media_profiles.xml
