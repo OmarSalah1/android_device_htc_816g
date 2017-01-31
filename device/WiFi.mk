9222e2c5933c14e7685cebbd2ef3c05f490dfdf0
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 05:19:20 +0530

    Device : Add WiFi.mk Configs

---
+++ device/WiFi.mk
@@ -0,0 +1,10 @@
+# WiFi
+PRODUCT_PACKAGES += \
+    lib_driver_cmd_mt66xx
+
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
