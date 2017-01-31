c17a66cdef41cbb0598a38cc7dbac60c821af5ce
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 04:28:30 +0530

    Keylayout : Copy Keylayout Configs

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -7,3 +7,10 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 # include the non-open-source counterpart to this file
 -include vendor/htc/816g/AndroidBoardVendor.mk
 
+# Device path
+LOCAL_PATH := device/htc/816g
+
+# copy keylayout overrides
+$(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
+    cp -pf $(LOCAL_PATH)/configs/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
+
