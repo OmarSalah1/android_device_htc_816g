b7c14ca7ef94e2c9d92af6eeaadb9da5e3affbf1
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 01:33:27 +0530

    Board : Add Architecture.mk Configs

---
+++ board/Architecture.mk
@@ -0,0 +1,14 @@
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+
+
+# Architecture Extensions
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+
