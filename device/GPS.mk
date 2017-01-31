407eafae4ee4ad710d1fd3514d6c4d75e18f351e
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 04:18:04 +0530

    Device : Add GPS.mk Configs

---
+++ device/GPS.mk
@@ -0,0 +1,10 @@
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
+
+PRODUCT_PACKAGES += \
+    gps.mt6592 \
+    YGPS
