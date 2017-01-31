84bc7b047ec47a56b4d5ab1f2358fa8d7df5973f
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 04:19:30 +0530

    Device : Add Hostapd.mk Configs

---
+++ device/Hostapd.mk
@@ -0,0 +1,6 @@
+# Hostapd
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
+
