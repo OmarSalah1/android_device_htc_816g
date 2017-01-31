b433b942a709ffd847bcdeec42516f80d91bc9fa
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 01:34:55 +0530

    Board : Add Device.mk Configs

---
+++ board/Device.mk
@@ -0,0 +1,8 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := a5mgp_dug,htc816g,816g
+
+# OTA assert board
+TARGET_OTA_ASSERT_BOARD := mt6592
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
