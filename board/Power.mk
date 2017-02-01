a7a2797f4280946f3d7e1f2e373a45795448d8d0
GMCadiom <gm.cadiom@gmail.com>
2017-02-01 05:36:06 +0530

    Offline Charging : Add Custom Res

--- board/Power.mk
+++ board/Power.mk
@@ -1,3 +1,12 @@
 # Healthd & Charging
 BOARD_CHARGER_SHOW_PERCENTAGE := true
-
+HEALTHD_ENABLE_TRICOLOR_LED := false
+RED_LED_PATH := /sys/class/leds/red/brightness
+GREEN_LED_PATH := /sys/class/leds/green/brightness
+BLUE_LED_PATH := /sys/class/leds/blue/brightness
+# Next lines, fix charging-mod in power off. It needs to modify the init.cpp, too.
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.mtk
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(LOCAL_PATH)/charger/images
