797d6eb2ffaf7f4ea1ff289dd60f9fdea9cd9832
GMCadiom <gm.cadiom@gmail.com>
2017-03-05 22:41:42 +0530

    Kernel : Add MKBOOTIMG_ARGS

--- board/Kernel.mk
+++ board/Kernel.mk
@@ -2,6 +2,7 @@
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE :=
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 
 # Kernel properties
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
